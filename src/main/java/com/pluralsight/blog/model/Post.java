07a443314f084503be63e0effbc7a3593d82ad26
Jeff Gilbert <Jeff.Gilbert@wolterskluwer.com>
2022-04-08 13:42:21 -0600

    Chapter 4 updates.

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -17,6 +17,10 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Version
+    private Long version;
+    @NotNull
+    @Size(min = 4, max = 100)
     private String title;
     @Column(length=1000000)
     @Lob
@@ -26,6 +30,7 @@ public class Post {
     private Date date;
     @ManyToOne(fetch = FetchType.EAGER)
     private Author author;
+
 
     public Post() {
         super();
